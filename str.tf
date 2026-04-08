19278858b9f79b96598e40f3cac853651edd8d0f
sushmitha <eekshithak334@gmail.com>
2026-04-08 14:51:01 +0530

    Update str.tf

--- str.tf
+++ str.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "portfolio_sa" {
-  name                     = "prasadportfoliosa88"
+  name                     = "sushmiportfoliosa88"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
@@ -13,7 +13,7 @@ resource "azurerm_storage_container" "images" {
 }
 
 resource "azurerm_storage_blob" "profile_photo" {
-  name                   = "prasad.jpg"
+  name                   = "sushmi.jpg"
   storage_account_name   = azurerm_storage_account.portfolio_sa.name
   storage_container_name = azurerm_storage_container.images.name
   type                   = "Block"
